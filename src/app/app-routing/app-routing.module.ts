e2bf06c9d6197628dc1538da55a80abb51a22bda
julianbusch <julian.busch@yahoo.de>
2019-06-27 15:04:41 +0200

    neue Fragen hinzugefügt

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -11,6 +11,36 @@ import {Frage7Component} from "../fragen/frage7/frage3.component";
 import {Frage8Component} from "../fragen/frage8/frage3.component";
 import {Frage9Component} from "../fragen/frage9/frage3.component";
 import {Frage10Component} from "../fragen/frage10/frage3.component";
+import {Frage11Component} from "../fragen/frage11/frage3.component";
+import {Frage12Component} from "../fragen/frage12/frage3.component";
+import {Frage13Component} from "../fragen/frage13/frage3.component";
+import {Frage14Component} from "../fragen/frage14/frage3.component";
+import {Frage15Component} from "../fragen/frage15/frage3.component";
+import {Frage16Component} from "../fragen/frage16/frage3.component";
+import {Frage17Component} from "../fragen/frage17/frage3.component";
+import {Frage18Component} from "../fragen/frage18/frage3.component";
+import {Frage19Component} from "../fragen/frage19/frage3.component";
+import {Frage20Component} from "../fragen/frage20/frage3.component";
+import {Frage21Component} from "../fragen/frage21/frage3.component";
+import {Frage22Component} from "../fragen/frage22/frage3.component";
+import {Frage23Component} from "../fragen/frage23/frage3.component";
+import {Frage24Component} from "../fragen/frage24/frage3.component";
+import {Frage25Component} from "../fragen/frage25/frage3.component";
+import {Frage26Component} from "../fragen/frage26/frage3.component";
+import {Frage27Component} from "../fragen/frage27/frage3.component";
+import {Frage28Component} from "../fragen/frage28/frage3.component";
+import {Frage29Component} from "../fragen/frage29/frage3.component";
+import {Frage30Component} from "../fragen/frage30/frage3.component";
+import {Frage31Component} from "../fragen/frage31/frage3.component";
+import {Frage32Component} from "../fragen/frage32/frage3.component";
+import {Frage33Component} from "../fragen/frage33/frage3.component";
+import {Frage34Component} from "../fragen/frage34/frage3.component";
+import {Frage35Component} from "../fragen/frage35/frage3.component";
+import {Frage36Component} from "../fragen/frage36/frage3.component";
+import {Frage37Component} from "../fragen/frage37/frage3.component";
+import {Frage38Component} from "../fragen/frage38/frage3.component";
+import {Frage39Component} from "../fragen/frage39/frage3.component";
+import {Frage40Component} from "../fragen/frage40/frage3.component";
 
 
 const routes: Routes = [
@@ -25,6 +55,37 @@ const routes: Routes = [
   {path: 'frage8', component: Frage8Component},
   {path: 'frage9', component: Frage9Component},
   {path: 'frage10', component: Frage10Component},
+  {path: 'frage11', component: Frage11Component},
+  {path: 'frage12', component: Frage12Component},
+  {path: 'frage13', component: Frage13Component},
+  {path: 'frage14', component: Frage14Component},
+  {path: 'frage15', component: Frage15Component},
+  {path: 'frage16', component: Frage16Component},
+  {path: 'frage17', component: Frage17Component},
+  {path: 'frage18', component: Frage18Component},
+  {path: 'frage19', component: Frage19Component},
+  {path: 'frage20', component: Frage20Component},
+  {path: 'frage21', component: Frage21Component},
+  {path: 'frage22', component: Frage22Component},
+  {path: 'frage23', component: Frage23Component},
+  {path: 'frage24', component: Frage24Component},
+  {path: 'frage25', component: Frage25Component},
+  {path: 'frage26', component: Frage26Component},
+  {path: 'frage27', component: Frage27Component},
+  {path: 'frage28', component: Frage28Component},
+  {path: 'frage29', component: Frage29Component},
+  {path: 'frage30', component: Frage30Component},
+  {path: 'frage31', component: Frage31Component},
+  {path: 'frage32', component: Frage32Component},
+  {path: 'frage33', component: Frage33Component},
+  {path: 'frage34', component: Frage34Component},
+  {path: 'frage35', component: Frage35Component},
+  {path: 'frage36', component: Frage36Component},
+  {path: 'frage37', component: Frage37Component},
+  {path: 'frage38', component: Frage38Component},
+  {path: 'frage39', component: Frage39Component},
+  {path: 'frage40', component: Frage40Component},
+
 
 
 ];
--- src/app/fragen/fragen.module.ts
+++ src/app/fragen/fragen.module.ts
@@ -12,10 +12,46 @@ import {Frage7Component} from "./frage7/frage3.component";
 import {Frage8Component} from "./frage8/frage3.component";
 import {Frage9Component} from "./frage9/frage3.component";
 import {Frage10Component} from "./frage10/frage3.component";
+import {Frage11Component} from "./frage11/frage3.component";
+import {Frage12Component} from "./frage12/frage3.component";
+import {Frage13Component} from "./frage13/frage3.component";
+import {Frage14Component} from "./frage14/frage3.component";
+import {Frage15Component} from "./frage15/frage3.component";
+import {Frage16Component} from "./frage16/frage3.component";
+import {Frage17Component} from "./frage17/frage3.component";
+import {Frage18Component} from "./frage18/frage3.component";
+import {Frage19Component} from "./frage19/frage3.component";
+import {Frage20Component} from "./frage20/frage3.component";
+import {Frage21Component} from "./frage21/frage3.component";
+import {Frage22Component} from "./frage22/frage3.component";
+import {Frage23Component} from "./frage23/frage3.component";
+import {Frage24Component} from "./frage24/frage3.component";
+import {Frage25Component} from "./frage25/frage3.component";
+import {Frage26Component} from "./frage26/frage3.component";
+import {Frage27Component} from "./frage27/frage3.component";
+import {Frage28Component} from "./frage28/frage3.component";
+import {Frage29Component} from "./frage29/frage3.component";
+import {Frage30Component} from "./frage30/frage3.component";
+import {Frage31Component} from "./frage31/frage3.component";
+import {Frage32Component} from "./frage32/frage3.component";
+import {Frage33Component} from "./frage33/frage3.component";
+import {Frage34Component} from "./frage34/frage3.component";
+import {Frage35Component} from "./frage35/frage3.component";
+import {Frage36Component} from "./frage36/frage3.component";
+import {Frage37Component} from "./frage37/frage3.component";
+import {Frage38Component} from "./frage38/frage3.component";
+import {Frage39Component} from "./frage39/frage3.component";
+import {Frage40Component} from "./frage40/frage3.component";
 
 @NgModule({
   declarations: [Frage1Component, Frage2Component, Frage3Component, Frage4Component, Frage5Component, Frage6Component,
-    Frage7Component, Frage8Component, Frage9Component, Frage10Component
+    Frage7Component, Frage8Component, Frage9Component, Frage10Component, Frage11Component, Frage12Component,
+    Frage13Component, Frage14Component, Frage15Component, Frage16Component, Frage17Component, Frage18Component,
+    Frage19Component, Frage20Component, Frage21Component, Frage22Component, Frage23Component, Frage24Component,
+    Frage25Component, Frage26Component, Frage27Component, Frage28Component, Frage29Component, Frage30Component,
+    Frage31Component, Frage32Component, Frage33Component, Frage34Component, Frage35Component, Frage36Component,
+    Frage37Component, Frage38Component, Frage39Component, Frage40Component
+
   ],
   imports: [
     CommonModule, FormsModule,
